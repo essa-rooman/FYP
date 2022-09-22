4e69f4a996b6ee71cb146de0f651a4f35991db13
essa-rooman <92358751+essa-rooman@users.noreply.github.com>
2022-09-22 18:18:47 +0500

    code

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const app = require("./app");
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+// Connecting to database
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: "tehzeeb-wooden-crafts",
+  api_key: "744141499868112",
+  api_secret: "V3d_ZZLUPZiqE_Pw4aN3xwqd7a8",
+  secure: true,
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import playStore from "../../../images/playstore.png";
+import appStore from "../../../images/Appstore.png";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="leftFooter">
+        <h4>DOWNLOAD OUR APP</h4>
+        <p>Download App for Android and IOS mobile phone</p>
+        <img src={playStore} alt="playstore" />
+        <img src={appStore} alt="Appstore" />
+      </div>
+
+      <div className="midFooter">
+        <h2>Tehzeeb Wooden Crafts</h2>
+        <p>High Quality is our first priority</p>
+
+        <p>Copyrights 2022 &copy;T.W.C </p>
+      </div>
+
+      <div className="rightFooter">
+        <h4>Follow Us</h4>
+        <a href="https://www.instagram.com/tehzeebwoodencraft">
+          <InstagramIcon className="instagramSvgIcon" />
+        </a>
+        <a href="https://www.facebook.com/tehzeebwoodencraft">Facebook</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
