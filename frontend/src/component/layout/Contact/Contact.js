36a7f78f3c806ed0f76e600fcee6ff0ead4ec2c1
essa-rooman <92358751+essa-rooman@users.noreply.github.com>
2022-09-27 00:38:17 +0500

    change

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <div className="contactContainer">
       <a className="mailBtn" href="mailto:tehzeebwoodencraft@gmail.com">
-        <Button>Contact:tehzeebwoodencraft@gmail.com</Button>
+        <Button>Contact: tehzeebwoodencraft@gmail.com</Button>
       </a>
     </div>
   );
